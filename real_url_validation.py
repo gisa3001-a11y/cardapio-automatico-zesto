f5265a884e7d5fe7e5b5dbbeaf71e9f2375a5e6e
gisa3001-a11y <gisa3001@gmail.com>
2026-08-30 11:00:32 -0300

    test: atualizar caso real do Ola Click

--- real_url_validation.py
+++ real_url_validation.py
@@ -29,7 +29,7 @@ CASOS = [
     ("Hubt", "https://www.hubt.com.br/oriental-suzano/"),
     ("MenuDino", "https://pollolokoouroverde.menudino.com/"),
     ("Neemo", "https://loja.neemo.com.br/braseiro-choperia-e-espetaria"),
-    ("Ola Click", "https://tatys-burger-2.ola.click/products"),
+    ("Ola Click", "https://eloni-bistro.ola.click/products"),
     ("Cardapio Web", "https://app.cardapioweb.com/shakepoint_westplaza"),
     ("EntregueJa", "https://vemdeburger.entregueja.com.br/home"),
     ("Saipos", "https://temperodaleia.saipos.com/"),
